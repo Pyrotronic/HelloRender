1f9331ac0fd5b3aa84bd2be6069fc322e681188f
Pyrotronic <132668216+Pyrotronic@users.noreply.github.com>
2024-12-11 15:04:16 +0300

    Update Program.cs

--- HelloRender/Program.cs
+++ HelloRender/Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
+//string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+//app.Urls.Add($"http://*:{port}");
 
 app.MapGet("/", () => "Hello, Render!");
 
