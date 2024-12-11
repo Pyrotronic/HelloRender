7d5c3615e3d1497449c1938c57234a5277aa40a3
Pyrotronic <132668216+Pyrotronic@users.noreply.github.com>
2024-12-11 17:22:02 +0300

    Delete HelloRender directory

--- HelloRender/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
-
-app.MapGet("/", () => "Hello, Render!");
-
-app.Run();
